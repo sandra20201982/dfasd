ceab2c7bee90cee762d03ce3d55e5fa3fbf64d47
sandra20201982 <70581812+sandra20201982@users.noreply.github.com>
2020-12-21 19:44:16 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,28 @@
+var canvas, backgroundImage;
+
+var gameState = 0;
+var playerCount;
+
+var database;
+
+var form, player, game;
+
+function preload()
+{
+  bg=loadImage("images/background.jpg");
+}
+
+function setup(){
+ // background(bg)
+
+  canvas = createCanvas(400,400);
+  database = firebase.database();
+  game = new Game();
+  game.getState();
+  game.start();
+}
+
+
+function draw(){
+  background(bg)
+}
